fe345031bddba2efc543dbe7bbc47d2a56690b0a
Pooja Kandoi <pooja.kandoi@thoughtworks.com>
2021-01-10 18:27:49 +0530

    [PK] ADD tail function and its tests

---
+++ src/tail.js
@@ -0,0 +1,12 @@
+function tail(arr){
+    var res=[];
+    if(arr.length==0)
+    return res;
+    else{
+        for(i=1;i<arr.length;i++){
+            res.push(arr[i]);
+        }
+        return res;
+    }
+}
+module.exports = tail;
